8916166c70e363fbba7c24d99abb01455ccb60b5
YinYihai <13541131574@163.com>
2020-03-12 22:43:37 +0800

    helloworld add commit

--- SuperPrime.c
+++ SuperPrime.c
@@ -1,10 +1,60 @@
 //超级素数：它本身，各位数字的和，各位数字的平方和，都是素数。
 //求 100~10000 内的所有超级素数，及它们的平均数。
+#include <stdio.h>
+#include <string.h>
 
-int isPrime(int x);
-int splitNum(int x, int num[]);
+int splitNumber[5] = {0};
+
+int isPrime(int x)
+{
+    if (x <= 1) return 0;
+    else
+    {
+        int flag = 1;
+        for (int i = 2; i < x; ++i)
+            if (!(x % i))
+            {
+                flag = 0;
+                break;
+            }
+        return flag;
+    }
+}
+int splitNum(int x, int* num)
+{
+    memset(num, 0, 5*sizeof(int));
+    int bit = 0;
+    while (x)
+    {
+        int i = x % 10;
+        num[bit++] =  i;
+        x /= 10;
+    }
+    return bit;
+}
+
 
 int main()
 {
+    int total = 0, cnt = 0;
+    for (int i = 10; i <= 10000; ++i)
+    {
+        int bit = splitNum(i, splitNumber);
+        int sum0 = 0, sum1 = 0;
+
+        for (int j = bit-1; j >= 0; --j)
+        {
+            sum0 += splitNumber[j];
+            sum1 += splitNumber[j]*splitNumber[j];
+        }
+        if (isPrime(i) && isPrime(sum0) && isPrime(sum1))
+        {
+            printf("%d\n", i);
+            total += i;
+            ++cnt;
+        }
+    }
+    printf("average = %f\n", total/(cnt*1.0));
+    printf("\n");
     return 0;
 }
--- SuperPrime.java
+++ SuperPrime.java
@@ -1,13 +1,38 @@
 //超级素数：它本身，各位数字的和，各位数字的平方和，都是素数。
 //求 100~10000 内的所有超级素数，及它们的平均数。
 
-class SuperPrime
-{
-	public static void main(String[] args)  
-	{
-		System.out.println(args); //[ljava.lang.string;@139a55  “[”代表数组， “l”代表long , "@139a55"代表哈希值
-		System.out.println(args.length);  //默认长度为0
-		for(int i = 0; i < args.length; i++)
-			System.out.println(args[i]); 
-	}
+public class SuperPrime {
+    static boolean isPrime(int x) {
+        if (x <= 1) return false;
+        else
+        {
+            boolean flag = true;
+            for (int i = 2; i < x; ++i) {
+                if (x%i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            return flag;
+        }
+    }
+    public static void main(String[] args) {
+        int total = 0; int cnt = 0;
+        for (int i = 10; i <= 10000; ++i)
+        {
+            int sum0 = 0, sum1 = 0;
+            String num = String.valueOf(i);
+            for (int j = 0; j < num.length(); ++j)
+            {
+                int n = (int)(num.charAt(j))-(int)('0');
+                sum0 += n;
+                sum1 += n*n;
+            }
+            if (isPrime(i) && isPrime(sum0) && isPrime(sum1)) {
+                System.out.println(i);
+                total += i; ++cnt;
+            }
+        }
+        System.out.println("average = " + total/(double)cnt);
+    }
 }
