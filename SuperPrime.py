ae53b00b970e7f20b086bc11785cbc1441eeef14
YinYihai <13541131574@163.com>
2020-03-13 09:52:27 +0800

    helloworld add commit

--- SuperPrime.py
+++ SuperPrime.py
@@ -2,3 +2,29 @@
 //求 100~10000 内的所有超级素数，及它们的平均数。
 
 //要安装python运行环境，注意python2与python3的区别挺大的
+def isPrime(x):
+    if x <= 1:
+        return False
+    else:
+        flag = True
+        for i in range(2, x):
+            if x % i == 0:
+                flag = False
+                break
+        return flag
+
+if __name__ == '__main__':
+    total = 0
+    cnt = 0
+    for x in range(10, 10001, 1):
+        s = str(x)
+        sum0, sum1 = 0, 0
+        for e in s:
+            n = int(e)
+            sum0 += n
+            sum1 += n**2
+        if isPrime(x) and isPrime(sum0) and isPrime(sum1):
+            print(x)
+            total += x;
+            cnt += 1;
+    print("average = ",total/cnt);
