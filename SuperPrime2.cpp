be621aed3c45a87123e41c93ddc5fbca856056b1
YinYihai <62104095+YinYihai@users.noreply.github.com>
2020-05-14 17:24:42 +0800

    <作业--继承关系实现相同功能> 完成版

--- SuperPrime2.cpp
+++ SuperPrime2.cpp
@@ -1,16 +1,16 @@
-//作业：面向对象设计以下框架的代码细节，程序能编译运行得到正确结果
+//作业：删除多余的集合类，使用继承方法实现相同的功能。 
 #include <iostream>
-class PrimeSet;
+
 class Prime {
-	friend class PrimeSet;
 public:
-	Prime() :
-			number(0) {
+	Prime() :number(0) {
 	}
-	Prime(int n) :
-			number(n) {
+	Prime(int n) :number(n) {
 	}
 	~Prime() {
+	}
+	operator int() const {
+		return number;
 	}
 	bool isPrime() {
 		if (number <= 1)
@@ -18,7 +18,7 @@ public:
 		else {
 			bool flag = true;
 			for (int i = 2; i < number; ++i) {
-				if (!(number % i)) {
+				if (!(number % 2)) {
 					flag = false;
 					break;
 				}
@@ -26,22 +26,20 @@ public:
 			return flag;
 		}
 	}
-	operator int() const {
-		return number;
-	}
 private:
 	const int number;
 };
+
 class PrimeSet {
 public:
 	PrimeSet(int size) {
-		//集合的构造什么？
-		set = new Prime*[size];
+		//集合的构造什么？ 
+		set = new Prime * [size];
 		this->size = size;
 		index = 0;
 	}
 	~PrimeSet() {
-		for (int i = 0; i < index; ++i)  //销毁对象
+		for (int i = 0; i < index; ++i)  //销毁对象 
 			delete set[i];
 		delete[] set;
 	}
@@ -52,11 +50,9 @@ public:
 				count += 1;
 		return count;
 	}
-
 	bool add(int n) {
-		if (index == size)
-			return false;
-		Prime *p = new Prime(n);
+		if (index == size)  return false;
+		Prime* p = new Prime(n);
 		set[index] = p;
 		index += 1;
 		return true;
@@ -67,36 +63,45 @@ public:
 				return false;
 		return true;
 	}
-	Prime** value() const {
-		return set;
+	int length() const {
+		return size;
 	}
+	Prime** FAddress() const {
+		return static_cast<Prime** const>(set);
+	}
+
+	bool addItem(Prime* item) { //这个函数既可以用于添加Prime对象，也可用于添加Prime的任何派生类对象。
+		if (index == size) return false;
+		set[index++] = item;
+		return true;
+	}
+
 private:
-	Prime **set;
+	Prime** set;
 	int size, index;
 };
-class SuperPrime: public Prime {
+
+class SuperPrime : public Prime {
 public:
-	SuperPrime() :
-			Prime(0), pset(3) {  //为什么必须有？
+	SuperPrime() :Prime(0), pset(3) {  //为什么必须有？ 
 	}
-	SuperPrime(int n) :
-			Prime(n), pset(3) {
+	SuperPrime(int n) :Prime(n), pset(3) {
 		// number split into N
 		int temp = n;
 		while (temp > 0) {
 			int t = temp % 10;
 			temp /= 10;
-			pset.add(t);  //作业：单个数字为对象？还是和/积/平方和为对象？
+			pset.add(t);  //作业：单个数字为对象？还是和/积/平方和为对象？ 
 		}
 	}
 	~SuperPrime() {
 	}
-	bool isSuperPrime() {
-		PrimeSet tempSet(3);
-		tempSet.add(sum());
-		tempSet.add(multi());
-		tempSet.add(squareSum());
-		if (Prime::isPrime() && tempSet.isAllPrime())
+	bool isPrime() {   //类/对象的接口，更抽象说是外观 
+		PrimeSet set(3); 
+		set.add(sum());
+		set.add(multi());
+		set.add(squareSum());
+		if (Prime::isPrime() && set.isAllPrime())
 			return true;
 		return false;
 	}
@@ -104,57 +109,26 @@ private:
 	PrimeSet pset;
 	int sum() {
 		int sum = 0;
-		for (int i = 0; i < 3; ++i)
-			sum += **(pset.value() + i);
+		for (int i = 0; i < pset.length(); ++i)
+			sum += *(pset.FAddress()[i]);
 		return sum;
 	}
 	int multi() {
 		int mul = 1;
-		for (int i = 0; i < 3; ++i)
-			mul *= **(pset.value() + i);
+		for (int i = 0; i < pset.length(); ++i)
+			mul *= *(pset.FAddress()[i]);
 		return mul;
 	}
 	int squareSum() {
 		int squ = 0;
-		for (int i = 0; i < 3; ++i)
-			squ += **(pset.value() + i) * **(pset.value() + i);
+		for (int i = 0; i < pset.length(); ++i)
+			squ += *(pset.FAddress()[i]) * *(pset.FAddress()[i]);
 		return squ;
 	}
 };
-class SuperPrimeSet {
-public:
-	SuperPrimeSet(int from, int to) {
-		size = to - from;
-		for (int i = from; i < to; i++)
-			set[i - from] = new SuperPrime(i);
-	}
-	~SuperPrimeSet() {
-		for (int i = 0; i < size; i++)
-			delete set[i];
-	}
-	int count() {
-		int count = 0;
-		for (int i = 0; i < size; i++)
-			if (set[i]->isSuperPrime())
-				count += 1;
-		return count;
-	}
-	int sum() {
-		int sum = 0;
-		/*
-		 for (int i = 0; i < size; i++)
-		 if(set[i].isSuperPrime())
-		 sum += set[i];
-		 */
-		return sum;
-	}
-private:
-	SuperPrime **set;
-	int size, index;
-};
 int main() {
 	SuperPrime sp(113);
-	if (sp.isSuperPrime())
+	if (sp.isPrime())
 		std::cout << "113 is SuperPrime" << std::endl;
 	else
 		std::cout << "113 is NOT SuperPrime" << std::endl;
