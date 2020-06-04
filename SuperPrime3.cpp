ad370e1bd3d1b11631526a6e0a1c379f0d04826c
YinYihai <62104095+YinYihai@users.noreply.github.com>
2020-06-04 23:13:07 +0800

    作业《整体框架  作业处理超大整体》带类模板的完成版3

--- SuperPrime3.cpp
+++ SuperPrime3.cpp
@@ -10,62 +10,62 @@ using namespace std;
 
 /*************************************************************/
 /* 这段代码用于进行大数的加法,乘法,除法 */
-int a[500],b[500],c[500],d[500],e[1000];
+int a[500], b[500], c[500], d[500], e[1000];
 
-string operator+(const string& s1, const string& s2) //字符串的加法
-{                                                    //这个似乎有点问题，因为 string 类本身就重载过加法运算符，这里再重载，感觉会有问题.
-    int m,n,v,t;
-    m=s1.length();
-    n=s2.length();
-    memset(a,0,sizeof(a));
-    memset(b,0,sizeof(b));
-    for(int i=0;i<m;i++){
-        a[m-1-i]=s1[i]-'0';
+string strAdd(const string& s1, const string& s2) //字符串的加法
+{   // 这里不进行运算符重载，是因为string类本身就进行过 “+” 的重载
+    int m, n, v, t;
+    m = s1.length();
+    n = s2.length();
+    memset(a, 0, sizeof(a));
+    memset(b, 0, sizeof(b));
+    for (int i = 0; i < m; i++) {
+        a[m - 1 - i] = s1[i] - '0';
     }
-    for(int i=0;i<n;i++){
-        b[n-1-i]=s2[i]-'0';
+    for (int i = 0; i < n; i++) {
+        b[n - 1 - i] = s2[i] - '0';
     }
-    m=max(m,n);
+    m = max(m, n);
     m++;
-    memset(c,0,sizeof(c));
-    for(int i=0;i<m;i++){
-        v=a[i]+b[i];
-        if((c[i]+v)<10)
-            c[i]+=v;
-        else{
-            c[i+1]+=(c[i]+v)/10;
-            c[i]=(c[i]+v)%10;
+    memset(c, 0, sizeof(c));
+    for (int i = 0; i < m; i++) {
+        v = a[i] + b[i];
+        if ((c[i] + v) < 10)
+            c[i] += v;
+        else {
+            c[i + 1] += (c[i] + v) / 10;
+            c[i] = (c[i] + v) % 10;
         }
     }
-    if(c[m-1]==0)    m--;
+    if (c[m - 1] == 0)    m--;
     string result;
-    for(int i=m-1;i>=0;i--) result.push_back(c[i]+'0');
+    for (int i = m - 1; i >= 0; i--) result.push_back(c[i] + '0');
     return result;
 }
 
 string operator*(const string& a, const string& b) //字符串的乘法
 {
-    int i,j,u,v,w;
-    u=a.length();
-    v=b.length();
-    memset(c,0,sizeof(c));
-    for(i=0;i<u;i++)
-        c[u-1-i]=a[i]-'0';
-    for(i=0;i<v;i++)
-        d[v-1-i]=b[i]-'0';
-    for(i=0;i<u;i++){
-        for(j=0;j<v;j++){
-            w=c[i]*d[j];
-            if(e[i+j]+w<10)    e[i+j]+=w;
-            else{
-                e[i+j+1]+=(e[i+j]+w)/10;
-                e[i+j]=(e[i+j]+w)%10;
+    int i, j, u, v, w;
+    u = a.length();
+    v = b.length();
+    memset(c, 0, sizeof(c));
+    for (i = 0; i < u; i++)
+        c[u - 1 - i] = a[i] - '0';
+    for (i = 0; i < v; i++)
+        d[v - 1 - i] = b[i] - '0';
+    for (i = 0; i < u; i++) {
+        for (j = 0; j < v; j++) {
+            w = c[i] * d[j];
+            if (e[i + j] + w < 10)    e[i + j] += w;
+            else {
+                e[i + j + 1] += (e[i + j] + w) / 10;
+                e[i + j] = (e[i + j] + w) % 10;
             }
         }
     }
-    for(i=u+v-1;i>0&&e[i]==0;i--);
+    for (i = u + v - 1; i > 0 && e[i] == 0; i--);
     string temp;
-    for(;i>=0;i--) temp.push_back(e[i]+'0');
+    for (; i >= 0; i--) temp.push_back(e[i] + '0');
     return temp;
 }
 
@@ -76,13 +76,13 @@ bool operator<(const string& s1, const string& s2)  // 字符串的大小比较
     else return s1 < s2;
 }
 
-bool compare(int a[],int b[])
+bool compare(int a[], int b[])
 {
-    if(a[0] != b[0])  return a[0]>b[0];
-    for(int i = a[0]; i>0 ;--i)
+    if (a[0] != b[0])  return a[0] > b[0];
+    for (int i = a[0]; i > 0; --i)
     {
-        if(a[i]!=b[i])
-            return a[i]>b[i];
+        if (a[i] != b[i])
+            return a[i] > b[i];
     }
     return true;
 }
@@ -95,36 +95,36 @@ operator/(const string& s1, const string& s2)  // 字符串的除法
     memset(c, 0, sizeof(c));
     a[0] = s1.length();
     b[0] = s2.length();
-    for(int i=1; i<=a[0]; ++i) a[i] = s1[a[0]-i]-'0';
-    for(int i=1; i<=b[0]; ++i) b[i] = s2[b[0]-i]-'0';
+    for (int i = 1; i <= a[0]; ++i) a[i] = s1[a[0] - i] - '0';
+    for (int i = 1; i <= b[0]; ++i) b[i] = s2[b[0] - i] - '0';
     c[0] = a[0] - b[0] + 1;
-    for(int i=c[0]; i>0; --i)
+    for (int i = c[0]; i > 0; --i)
     {
-        memset(d,0,sizeof(d));
-        for(int j=1;j<=b[0];j++)
-            d[j+i-1]=b[j];
+        memset(d, 0, sizeof(d));
+        for (int j = 1; j <= b[0]; j++)
+            d[j + i - 1] = b[j];
         d[0] = b[0] + i - 1;
-        while(compare(a,d))
+        while (compare(a, d))
         {
             c[i]++;
-            for(int i=1;i<=a[0];i++)
+            for (int i = 1; i <= a[0]; i++)
             {
-                a[i]-=d[i];
-                if(a[i]<0)
+                a[i] -= d[i];
+                if (a[i] < 0)
                 {
-                    a[i+1]--;
-                    a[i]+=10;
+                    a[i + 1]--;
+                    a[i] += 10;
                 }
             }
-            while(a[0]>0&&a[a[0]]==0)    a[0]--;
+            while (a[0] > 0 && a[a[0]] == 0)    a[0]--;
         }
     }
-    while(c[0]>1&&c[c[0]]==0)    c[0]--;
+    while (c[0] > 1 && c[c[0]] == 0)    c[0]--;
     string Quotient;
-    for(int i=c[0];i>0;--i)  Quotient.push_back(c[i]+'0');
+    for (int i = c[0]; i > 0; --i)  Quotient.push_back(c[i] + '0');
     string Remainder;
-    for(int i=a[0];i>0;--i)  Remainder.push_back(a[i]+'0');
-    return {Quotient, Remainder};
+    for (int i = a[0]; i > 0; --i)  Remainder.push_back(a[i] + '0');
+    return { Quotient, Remainder };
 }
 /*************************************************************/
 
@@ -137,7 +137,7 @@ public:
         else if (num < "2") return false;
         else
         {
-            for (string s = "2";  s < num; s = s + string("1"))
+            for (string s = "2"; s < num; s = strAdd(s, "1"))
             {
                 auto temp = num / s;
                 if (temp.first == string("0"))
@@ -146,6 +146,7 @@ public:
             return true;
         }
     }
+    virtual ~BigPrime() {}
     const string valueOf() {
         return num;
     }
@@ -156,15 +157,15 @@ protected:
 class BigSuperPrime : public BigPrime {
 public:
     BigSuperPrime(string n = string("")) :
-            BigPrime(n), sum("0"), mul("1"), squ("0") {  }
-    virtual bool isPrime()
+        BigPrime(n), sum("0"), mul("1"), squ("0") {  }
+    virtual bool isPrime() override
     {
         for (int i = 0; i < num.length(); ++i)
         {
             string temp; temp.push_back(num[i]);
-            sum = sum + temp;
+            sum = strAdd(sum, temp);
             mul = sum * temp;
-            squ = sum + temp * temp;
+            squ = strAdd(squ, temp*temp);
         }
         BigPrime b0(sum);
         BigPrime b1(mul);
@@ -179,12 +180,13 @@ private:
     string squ;
 };
 
+template <typename T>
 class Set {
 public:
     Set(int sz);
     ~Set();
-    bool add(BigPrime* bp);
-    bool remove(BigPrime* bp);
+    bool add(T* bp);
+    bool remove(T* bp);
     int count() const {
         int ret = 0;
         for (int i = 0; i < index; i++) {
@@ -197,12 +199,12 @@ public:
         return 0;
     }
 private:
-    BigPrime** pset;
+    T** pset;
     int size, index;
 };
 
 int main() {
-    Set set(1000);
+    Set<BigPrime> set(1000);
     BigSuperPrime bp("212121221111111212211211212221"), bp1("3231122122222212111111111112221221");
     set.add(&bp);
     set.add(&bp1);
@@ -210,15 +212,19 @@ int main() {
     return 0;
 }
 
-Set::Set(int sz) :size(sz) {
+template <typename T>
+Set<T>::Set(int sz) :size(sz) {
     index = 0;
-    pset = new BigPrime * [size];  //分配存储空间
+    pset = new T * [size];  //分配存储空间
 }
 
-Set::~Set() {
+template <typename T>
+Set<T>::~Set() {
     delete[] pset;  //回收
 }
-bool Set::add(BigPrime* bp) {
+
+template <typename T>
+bool Set<T>::add(T* bp) {
     pset[index++] = bp;
     return true;
 }
